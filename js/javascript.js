e35b8ee2b1d6cd78e88472905f6ff5ed6259b278
migdood <almigdadbolad@gmail.com>
2023-08-15 01:13:27 +0200

    holy moly after so much blood, sweat and tears it's working

--- js/javascript.js
+++ js/javascript.js
@@ -1,49 +1,65 @@
-function playRockPaperScissors(){
-    const rockPaperScissors = ["rock","paper", "scissors"];
+function BotChoice(){
+    let choices = ["rock","paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+let playerWonRounds = 0;
+let botWonRounds = 0;
+let roundsToWin = 5;
 
-    function botChoice(){
-    let result = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
-    
-    return result.toLowerCase();
-    }
-
-    function roundResult(bot, player){
+function BotVSPlayer(bot, player){
+        let result;
         if((bot == "rock" && player == "scissors") ||
         (bot == "scissors" && player == "paper") ||
         (bot == "paper" && player == "rock"))
         {
-            return "Bot Won."
+            botWonRounds++;
+            result = "Bot Won.";
         }
         else if((player == "rock" && bot == "scissors") ||
         (player == "scissors" && bot == "paper") ||
         (player == "paper" && bot == "rock"))
         {
-            return "Player Won."
+            playerWonRounds++;
+            result = "Player Won."
         }
         else if(bot == player)
         {
-            return "Draw."
+            result = "Draw."
         }
         else
         {
-            return "Wrong input.";
+            result = "Wrong input.";
         }
-    }
 
-    let playerChoice = prompt("You will be playing best of 5 rounds. \nPlease Enter Rock, Paper, or Scissors");
-
-    let stringBotChoice = botChoice();
-    
-    let winner = roundResult(stringBotChoice, playerChoice);
-
-    console.log("Bot: " + stringBotChoice +"\nPlayer: "+playerChoice +"\n"+ winner);
-
-    return winner;
+        return `Bot: ${bot}\nPlayer: ${player}\nConclusion: ${result}`;    
 }
 
+function PlayingRounds() {
+    while (playerWonRounds < roundsToWin && botWonRounds < roundsToWin) {
+        let playerChoice = prompt("Please enter Rock, Paper, or Scissors");
+        if (playerChoice === null) {
+            console.log("Game canceled by the player.");
+            return;
+        }
+        
+        if (!["rock", "paper", "scissors"].includes(playerChoice.toLowerCase())) {
+            console.log("Invalid choice. Please enter Rock, Paper, or Scissors.");
+            continue;
+        }
+        
+        let botChoiceVar = BotChoice();
+        console.log(`${BotVSPlayer(playerChoice.toLowerCase(), botChoiceVar)}\nBot Score: ${botWonRounds}\nPlayer Score: ${playerWonRounds}`);
+    }
 
+    if (playerWonRounds >= roundsToWin) {
+        console.log("Player wins the game!");
+    } else {
+        console.log("Bot wins the game!");
+    }
 
+}
 
+PlayingRounds();
 /*
 let answer = parseInt(prompt("Enter a number to  be FizzBuzzed: "));
 
