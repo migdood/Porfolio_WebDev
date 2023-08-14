b3d4d333ff4b70f1ccce72dd9ed57f9e229b06b8
migdood <almigdadbolad@gmail.com>
2023-08-14 20:52:15 +0200

    struggling so hard, optimized the js.

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,49 @@
-const rockPaperScissors = ["rock","paper", "scissors"];
-const playerChoice = "rock";
+function playRockPaperScissors(){
+    const rockPaperScissors = ["rock","paper", "scissors"];
 
-function botChoice(){
+    function botChoice(){
     let result = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
     
     return result.toLowerCase();
+    }
+
+    function roundResult(bot, player){
+        if((bot == "rock" && player == "scissors") ||
+        (bot == "scissors" && player == "paper") ||
+        (bot == "paper" && player == "rock"))
+        {
+            return "Bot Won."
+        }
+        else if((player == "rock" && bot == "scissors") ||
+        (player == "scissors" && bot == "paper") ||
+        (player == "paper" && bot == "rock"))
+        {
+            return "Player Won."
+        }
+        else if(bot == player)
+        {
+            return "Draw."
+        }
+        else
+        {
+            return "Wrong input.";
+        }
+    }
+
+    let playerChoice = prompt("You will be playing best of 5 rounds. \nPlease Enter Rock, Paper, or Scissors");
+
+    let stringBotChoice = botChoice();
+    
+    let winner = roundResult(stringBotChoice, playerChoice);
+
+    console.log("Bot: " + stringBotChoice +"\nPlayer: "+playerChoice +"\n"+ winner);
+
+    return winner;
 }
-let botboy = botChoice();
 
 
-function winner(bot, player){
-    if((bot == "rock" && player == "scissors") ||
-    (bot == "scissors" && player == "paper") ||
-    (bot == "paper" && player == "rock"))
-    {
-        return "Bot Won."
-    }
-    else if(bot == player)
-    {
-        return "Draw."
-    }
-    else
-    {
-        return "Player Won.";
-    }
-}
 
-function playRound(roundNum){
-    for (let i = 1; i <= roundNum; i++)
-    {
-        console.log(winner(botChoice(), playerChoice));
-    }
-}
 
-console.log(playRound(5));
 /*
 let answer = parseInt(prompt("Enter a number to  be FizzBuzzed: "));
 
