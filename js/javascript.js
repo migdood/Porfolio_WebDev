a55481722de201bcfa74ac20c5925886bfa55b7b
migdood <almigdadbolad@gmail.com>
2023-09-22 15:59:47 +0300

    there was a chance the image was cached so it didn't execute the JS properly. Fixed.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,13 @@
-document.getElementById("heroImageID").addEventListener("load", function () {
+function initializeImageAnimation() {
   const image = document.getElementById("heroImageID");
 
   image.style.height = "300px";
-});
+}
+
+// Attach the event listener to the image load event
+document.getElementById("heroImageID").addEventListener("load", initializeImageAnimation);
+
+// Call the function immediately in case the image is already cached
+if (document.getElementById("heroImageID").complete) {
+  initializeImageAnimation();
+}
